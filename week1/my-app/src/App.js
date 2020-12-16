0c6a8f8444c2e7ca3e2995c9aaf973dc5c4affb2
bryan-huffstutler <bryan.huffstutler@bryanuniversity.edu>
2020-12-15 21:12:25 -0600

    Todo Part 1

---
+++ week1/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <input type="checkbox" />
+      <p>Paragraph El Numero Uno</p>
+
+      <input type="checkbox" />
+      <p>Paragraph El Numero Dos</p>
+
+      <input type="checkbox" />
+      <p>Paragraph El Numero Tres</p>
+
+      <input type="checkbox" />
+      <p>Paragraph El Numero Quatro</p>
+    </div>
+  );
+}
+
+export default App;
