cf1acd5a5c430ad4f3f012c2d1a0a846f92b5200
bryan-huffstutler <bryan.huffstutler@bryanuniversity.edu>
2021-02-07 19:30:29 -0600

    capston

---
+++ week7/capstone/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../compstyles/footer.css'
+
+function Footer () {
+  return (
+    <div id="footer">
+      <footer>WEBDEV STUDENT 2020</footer>
+    </div>
+  )
+}
+
+export default Footer
